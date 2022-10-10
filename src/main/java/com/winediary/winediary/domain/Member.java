0f744afd99a11452ba975bcc8971d6f3c1f34115
SHIN <shs2810@gmail.com>
2022-10-10 21:08:28 +0900

    Jwt 적용 -1

--- src/main/java/com/winediary/winediary/domain/Member.java
+++ src/main/java/com/winediary/winediary/domain/Member.java
@@ -1,15 +1,18 @@
 package com.winediary.winediary.domain;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity
-public class Member extends BaseEntity{
+public class Member extends BaseEntity implements UserDetails {
     @Id
     @GeneratedValue
     @Column(name = "member_no")
@@ -38,4 +41,44 @@ public class Member extends BaseEntity{
 
     @Column
     private LocalDateTime lastLoginDateTime;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<String> memberRoles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.memberRoles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.name;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
---
+++ src/main/java/com/winediary/winediary/domain/MemberRoles.java
@@ -0,0 +1,7 @@
+package com.winediary.winediary.domain;
+
+public enum MemberRoles {
+    ROLE_ADMIN,
+    ROLE_USER,
+    ROLE_GUEST
+}
